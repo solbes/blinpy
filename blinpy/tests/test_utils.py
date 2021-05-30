5494da8cc3b2cf5c381d57c1d380104f46df3efe
Antti Solonen <antti.solonen@gmail.com>
2021-05-30 15:46:47 +0300

    test case for linfit with priors

--- blinpy/tests/test_utils.py
+++ blinpy/tests/test_utils.py
@@ -10,11 +10,19 @@ y = np.array([5.0, 5.0, 5.1, 5.3, 5.5, 5.7, 6.0, 6.3, 6.7, 7.1, 7.5])
 @pytest.mark.parametrize(
     "obs,A,kwargs,expected_mu",
     [
+        # line fit, no priors
         (
             y,
             np.concatenate((np.ones((11, 1)), x[:, np.newaxis]), axis=1),
             {},
             np.array([4.883977, 0.270029])
+        ),
+        # line fit, priors
+        (
+            y,
+            np.concatenate((np.ones((11, 1)), x[:, np.newaxis]), axis=1),
+            {'pri_mu': [4.0, 0.35], 'pri_cov': [1.0, 0.001]},
+            np.array([4.5468256, 0.3444257])
         )
     ]
 )
