47712c6bef3a8964279a83109cb366882fcf371d
Antti Solonen <antti.solonen@gmail.com>
2021-06-05 16:04:45 +0300

    test for GamModel

--- blinpy/tests/test_models.py
+++ blinpy/tests/test_models.py
@@ -54,3 +54,48 @@ def test_linear_model():
         lm.post_mu,
         rtol=1e-5
     )
+
+
+def test_gam_line_fit():
+
+    # 1) line fit, no priors
+    gam_specs = [{
+            'fun': lambda df: df['x'].values[:, np.newaxis],
+            'name': 'slope'
+        },
+        {
+            'fun': lambda df: np.ones((len(df),1)),
+            'name': 'bias'
+        }
+    ]
+
+    post_mu = models.GamModel('y', gam_specs).fit(data).post_mu
+    np.testing.assert_allclose(
+        np.array([0.270029, 4.883977]),
+        post_mu,
+        rtol=1e-5
+    )
+
+    # 2) partial prior
+    gam_specs = [{
+        'fun': lambda df: df['x'].values[:, np.newaxis],
+        'name': 'slope',
+        'prior': {
+            'B': np.eye(1),
+            'mu': np.array([0.35]),
+            'cov': np.array([0.001])
+        }
+    },
+        {
+            'fun': lambda df: np.ones((len(df), 1)),
+            'name': 'bias'
+        }
+    ]
+
+    post_mu = models.GamModel('y', gam_specs).fit(data).post_mu
+    np.testing.assert_allclose(
+        np.array([0.34251082265349875, 4.603935457929664]),
+        post_mu,
+        rtol=1e-5
+    )
+
