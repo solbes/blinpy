c64553bdef87687fdd33d6d2bdd1cdc3f4c8616b
Antti Solonen <antti.solonen@gmail.com>
2021-06-06 06:21:11 +0300

    add stuff to setup.py

--- setup.py
+++ setup.py
@@ -2,15 +2,23 @@ from setuptools import setup
 
 setup(
     name='blinpy',
-    version='',
+    version='0.1',
     packages=['blinpy'],
-    url='',
-    license='',
-    author='solant',
-    author_email='',
-    description='',
+    url='https://github.com/solbes/blinpy',
+    license='MIT',
+    author='Antti Solonen',
+    author_email='antti.solonen@gmail.com',
+    description='Bayesian Linear Models in Python',
+    keywords=['bayes', 'linear', 'gam'],
     install_requires=['numpy', 'pandas', 'jsonpickle'],
     extras_require={
         'dev': ['pytest']
-    }
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Data Scientists',
+        'Topic :: Data Science :: Linear Models',
+        "License :: OSI Approved :: MIT License",
+        'Programming Language :: Python :: 3',
+    ]
 )
