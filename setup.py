5cdd974d654569e33bb10bba529a326de2fbe739
Antti Solonen <antti.solonen@gmail.com>
2024-09-27 11:14:53 +0300

    modify setup.py

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ with open(os.path.join(os.path.dirname(__file__), "README.md")) as fh:
 
 setup(
     name='blinpy',
-    version='0.1.11',
+    version='0.1.12',
     packages=['blinpy'],
     url='https://github.com/solbes/blinpy',
-    download_url='https://github.com/solbes/blinpy/archive/refs/tags/0.1.11.tar.gz',
+    download_url='https://github.com/solbes/blinpy/archive/refs/tags/0.1.12.tar.gz',
     license='MIT',
     author='Antti Solonen',
     author_email='antti.solonen@gmail.com',
